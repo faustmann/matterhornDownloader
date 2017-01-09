da75c8b3b44fd3e599de72d051f2aa0424b1e480
Georg Faustmann <faustmann@gmail.com>
2017-01-09 18:47:04 +0100

    resume interrupted download

--- downMatterhorn.sh
+++ downMatterhorn.sh
@@ -11,7 +11,7 @@ do
     id=$(echo "$var" | grep -o 'id=.*')
     url=$(curl -s "https://mh-engage.ltcc.tuwien.ac.at/search/episode.xml?$id" | xmlindent -i 4 -f | grep "<url>https.*Screen.flv" | sed -e 's/<url>//' | xargs)
     echo "test $url"
-    wget -O $name $url
+    wget -c -O $name $url
     cd ..
 done
 
